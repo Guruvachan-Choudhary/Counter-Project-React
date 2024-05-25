760706ba4f294868d964dc5c6edc8bc0906ac416
Guruvachan Choudhary <108112127+Guruvachan-Choudhary@users.noreply.github.com>
2024-05-25 16:42:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     setCount(0);
   }
   return (
-    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#2c313c] flex-col gap-10">
       <div className="text-[#0398d4] font-medium text-2xl">
         Increment & Decrement
       </div>
